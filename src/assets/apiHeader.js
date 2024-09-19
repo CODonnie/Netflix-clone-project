f46af9226f4a294125ccc0ba9606b88fd1070cae
CODonnie <ordudesigns@gmail.com>
2024-09-19 04:12:48 -0700

    api header

---
+++ src/assets/apiHeader.js
@@ -0,0 +1,6 @@
+ const apiHeader = {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YWM2OWQ2ZTA5NDU5Y2NhNmQ1NzBmMGE0MWM3ZDgyMyIsIm5iZiI6MTcyNjQ3MzUxMy4zMDcyMDYsInN1YiI6IjY2ZTcxNjZlZTgyMTFlY2QyMmIwYTBjYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Z6KJiCdBKzGzSCj15I4ut_9eTHdnoVLx_LKjaLYb8hM'
+  }
+
+  export default apiHeader
